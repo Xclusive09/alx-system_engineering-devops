80e0196ec5e681eef9f82699a4dc0049a6498982
Abdulquddus Ibrahim <Ibrahimabdulquddus51@gmail.com>
2023-03-29 23:35:13 +0100

    install nginx with puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,26 @@
+#insall nginx web server with puppet
+
+exec { 'update system':
+    command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+    ensure => 'installed',
+    require => Exec['update system,]
+}
+
+file { '/var/www/html/index.html':
+    content => 'Hello World!'
+}
+
+exec {'redirect_me':
+    command => 'sed -i "24\     rewrite ^/redirect https://www.youtube.com/watch?v=QH2-TGUlwu4;" /etc/nginx/sites-available/default',
+    provider => 'shell'
+
+}
+
+service { 'nginx':
+ensure => running,
+require => Package['nginx']
+
+}
