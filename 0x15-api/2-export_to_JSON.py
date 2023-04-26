13bb320ff7797fde7d56c08d563d0e0b4f6fe479
Abdulquddus Ibrahim <Ibrahimabdulquddus51@gmail.com>
2023-04-26 22:52:32 +0100

    API

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,23 @@
+#!/usr/bin/ptython3
+
+"""
+Export employee data to json file
+"""
+
+import requests
+import sys
+import json
+
+if __name__ =="__main__":
+     user_id = sys.argv[1]
+     url = "https://jsonplaceholder.typicode.com/"
+     user = requests.get(url + "users/{}".format(user_id)).json()
+     username = user.get("username")
+     todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+     with open("{}.json".format(user_id), "w") as jsonfile:
+         json.dump({user_id: [{
+             "task": t.get("tittle"),
+             "completed": t.get("completed"),
+             "username": username
+             } for t in todos]}, jsonfile)
